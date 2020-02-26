cd273d3659484130a4f8c6c2ac6f93b8dd2ea90d
Jonah Gorodensky <57234145+jonahgorodensky@users.noreply.github.com>
2020-02-25 20:28:50 -0500

    First commit: trying to find a way to get all the entries (seem to only get a fraction)

---
+++ open_fda.R
@@ -0,0 +1,36 @@
+devtools::install_github("ropenhealth/openfda")
+
+library(openfda)
+
+x = fda_query("/drug/ndc.json") %>% 
+    fda_api_key("kwBweTyY0zxYfj27l7t6P7o68nSdxYaBspGyoBBy") %>% 
+    fda_limit(5) %>% 
+    fda_filter("generic_name","ustekinumab") %>% 
+    fda_search() %>% 
+    fda_exec()
+# str_c(x, )
+
+%>% 
+    fda_exec()
+y = fda_query("/drug/ndc.json") %>% 
+    fda_api_key("kwBweTyY0zxYfj27l7t6P7o68nSdxYaBspGyoBBy") %>% 
+    fda_filter("generic_name", "methotrexate") %>% 
+    fda_count("generic_name") %>% 
+    fda_exec()
+y = fda_query("/drug/ndc.json") %>% 
+    fda_api_key("kwBweTyY0zxYfj27l7t6P7o68nSdxYaBspGyoBBy") %>% 
+    # fda_filter("generic_name", "methotrexate") %>% 
+    fda_count("brand_name") %>% 
+    fda_exec()
+
+# https://api.fda.gov/drug/label.json?search=humira&skip=3&api_key=kwBweTyY0zxYfj27l7t6P7o68nSdxYaBspGyoBBy
+#this gives 15
+
+
+# Need to figure out how to get all the (all the entries) into the data
+
+
+df <- as_tibble(x) %>% 
+    select(-openfda)
+
+looking <- read_tsv("Desktop/package.txt")
