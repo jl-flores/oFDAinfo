4b9e43f53993b5145bf1d9c0d9a500b2a93cd623
Jonah Gorodensky <57234145+jonahgorodensky@users.noreply.github.com>
2020-03-02 17:28:52 -0500

    changed the query function to have a skip parameter. In conjunction I added a loop which allows for us to keep pulling data on a drug

--- open_fda_functions.R
+++ open_fda_functions.R
@@ -16,21 +16,41 @@ find_ndc <- function(name.category, name) {
 
 
 
-query <- function(search.list, search.drug) {
+query <- function(search.list, search.drug, limit = 100, skip = 0) {
     # this function takes a search.list (like "generic_name" or "brand_name")
     # and then the name of the drug you are interested in and outputs a
     # dataframe from the openfda
     require(openfda)
     require(dplyr)
     
-    query_text  <- fda_query("/drug/ndc.json") %>% 
+    .query_text  <- function(search.list, search.drug, limit = 100, skip){
+        fda_query("/drug/ndc.json") %>% 
         fda_api_key("kwBweTyY0zxYfj27l7t6P7o68nSdxYaBspGyoBBy") %>% 
-        fda_limit(100) %>% 
+        fda_limit(limit) %>% 
+        fda_skip(skip) %>% 
         fda_filter(search.list, search.drug) %>% 
         fda_search() %>% 
         fda_exec()
-    return(query_text)
+    }
     
+    queried <- .query_text(search.list, search.drug, skip = 0)
+    # looping to get all data
+    if (nrow(queried) != 100) {
+        return(queried)
+    } else {
+        skip <- limit
+        output_list <- vector("list", length = 100)
+        output_list[[1]] <- queried
+        index <- 2
+        while (nrow(queried) == 100) {
+            output_list[[index]] <- .query_text(search.list, search.drug, skip = skip)
+            queried <- output_list[[index]]
+            skip <- 100 * index
+            index <- index + 1
+        } 
+        return(output_list)
+    }  
+     
 }
 
 
