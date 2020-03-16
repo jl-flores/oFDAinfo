cd0c6a58e35c5f6b5f1d4d76fecb67e11a27318d
Jonah Gorodensky <57234145+jonahgorodensky@users.noreply.github.com>
2020-03-16 13:09:22 -0400

    ndc_query changed from require to ::

--- R/oxy-making_exportable_data.R
+++ R/oxy-making_exportable_data.R
@@ -55,17 +55,17 @@ openfda_tidydf <- function(data.frame.drug) {
 #'  #EXAMPLE1
 #'  }
 #' }
+#' @seealso 
+#'  \code{\link[purrr]{map}}
 #' @rdname ndc_query
 #' @export 
+#' @importFrom purrr map
 ndc_query <- function(list.names, append.existing = FALSE, 
                       path_output = "output/ndc_codes.csv",
                       csv = TRUE) {
     # The workhorse function: run it with a list of names of the format c("name
     # type", "drug name") and it will output the csv (if csv = true) or else
     # just a df (csv = false)
-    require(purrr)
-    require(dplyr)
-    require(readr)
 
     num_drugs <- length(list.names)
     combined_df <- vector("list", length = num_drugs)
@@ -78,16 +78,16 @@ ndc_query <- function(list.names, append.existing = FALSE,
         if(output_type){
             df_drug_clean <- openfda_tidydf(df_drug)
         }else {
-            df_drug_clean <- map(df_drug, openfda_tidydf)
-            df_drug_clean <- do.call(bind_rows, df_drug_clean)
+            df_drug_clean <- purrr::map(df_drug, openfda_tidydf)
+            df_drug_clean <- do.call(bind.rows, df_drug_clean)
         }
         combined_df[[ind_drug]] <- df_drug_clean
     }
     
-    combined_df <- bind_rows(combined_df)
+    combined_df <- bind.rows(combined_df)
     
     if(csv) {
-        write_csv(combined_df, append = append.existing, path = path_output)
+        write.csv(combined_df, append = append.existing, file = path_output)
     } 
     return(combined_df)
 }
