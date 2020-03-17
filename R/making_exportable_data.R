98ac40544210543141df7e8d5ab719dc5765282d
Jonah Gorodensky <57234145+jonahgorodensky@users.noreply.github.com>
2020-03-17 11:39:28 -0400

    Added roxygen documentation

--- R/making_exportable_data.R
+++ R/making_exportable_data.R
@@ -1,34 +1,29 @@
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param data.frame.drug PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Cleans the data
+#' @description Cleans the data in a reproducible manner, creating dataframes with the same columns for each pull from the API
+#' @param data.frame.drug List of API pulls (output of the find_ndc function)
+#' @return Clean data with consistent columns
+#' @details This function works on one dataframe. To work on a list of dataframes, as is often the output of \code{\link{[oFDAinfo]{find_ndc}}} must map this function over the list
 #' @examples 
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' z <- find_ndc("generic", "ustekinumab")
+#' openfda_cleandf(z)
 #' }
 #' @seealso 
 #'  \code{\link[dplyr]{mutate}},\code{\link[dplyr]{select}}
 #'  \code{\link[tidyselect]{select_helpers}}
 #' @rdname openfda_tidydf
-#' @export 
 #' @importFrom dplyr mutate select
 #' @importFrom tidyselect any_of
-openfda_tidydf <- function(data.frame.drug) {
-    # This function takes the output of the query as input and turns the data
-    # into a replicable dataframe that can then be combined so that all the
-    # drugs can be together.
-    require(dplyr)
-        
+openfda_cleandf <- function(data.frame.drug) {
+
     delisted <- data.frame.drug %>% 
         dplyr::mutate(package_ndc = as.character(packaging$package_ndc),
                marketing_start_date = as.character(packaging$marketing_start_date),
                description = as.character(packaging$description),
-               sample = as.character(packaging$sample)) %>% 
-            dplyr::select(-packaging)
+               sample = as.character(packaging$sample)
+               ) %>% 
+        dplyr::select(-packaging)
     renamed <- delisted %>% 
         dplyr::select(tidyselect::any_of(c("generic_name","brand_name", "package_ndc", 
                                     "product_ndc", "brand_name_base","labeler_name",
@@ -43,19 +38,18 @@ openfda_tidydf <- function(data.frame.drug) {
 }
 
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param list.names PARAM_DESCRIPTION
-#' @param append.existing PARAM_DESCRIPTION, Default: FALSE
-#' @param path_output PARAM_DESCRIPTION, Default: 'output/ndc_codes.csv'
-#' @param csv PARAM_DESCRIPTION, Default: TRUE
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @title Run a query of the API and output a clean, usable dataframe
+#' @description Run a query of the FDA's API in its entirety and output the data as a dataframe
+#' @param list.names a list of query parameters
+#' @param append.existing Do you wish to add the output to an already created text file Default: FALSE
+#' @param path.output Where would you like to output the csv, Default: 'output/ndc_codes.csv'
+#' @param csv Do you want to output a csv. If false, \code{path.output} and \code{append.existing} is irrelevant, Default: FALSE
+#' @return Returns a dataframe with all the results of the query 
+#' @details This is the workhorse function of the \code{oFDAinfo} package
 #' @examples 
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' drugs <- list(c("generic_name", "ibuprofen"), c("brand_name", "humira"))
+#' ndc_query(drugs, csv = TRUE)
 #' }
 #' @seealso 
 #'  \code{\link[purrr]{map}}
@@ -64,12 +58,9 @@ openfda_tidydf <- function(data.frame.drug) {
 #' @importFrom purrr map
 #' @importFrom dplyr bind_rows
 ndc_query <- function(list.names, append.existing = FALSE, 
-                      path_output = "output/ndc_codes.csv",
-                      csv = TRUE) {
-    # The workhorse function: run it with a list of names of the format c("name
-    # type", "drug name") and it will output the csv (if csv = true) or else
-    # just a df (csv = false)
-
+                      path.output = "output/ndc_codes.csv",
+                      csv = FALSE) {
+    
     num_drugs <- length(list.names)
     combined_df <- vector("list", length = num_drugs)
 
@@ -79,9 +70,9 @@ ndc_query <- function(list.names, append.existing = FALSE,
         df_drug <- find_ndc(param, drug)
         output_type <- inherits(df_drug, "data.frame")
         if(output_type){
-            df_drug_clean <- openfda_tidydf(df_drug)
+            df_drug_clean <- openfda_cleandf(df_drug)
         }else {
-            df_drug_clean <- purrr::map(df_drug, openfda_tidydf)
+            df_drug_clean <- purrr::map(df_drug, openfda_cleandf)
             df_drug_clean <- do.call(bind_rows, df_drug_clean)
         }
         combined_df[[ind_drug]] <- df_drug_clean
@@ -90,7 +81,7 @@ ndc_query <- function(list.names, append.existing = FALSE,
     combined_df <- bind_rows(combined_df)
     
     if(csv) {
-        write.csv(combined_df, append = append.existing, file = path_output)
+        write.csv(combined_df, append = append.existing, file = path.output)
     } 
     return(combined_df)
 }
--- R/open_fda_functions.R
+++ R/open_fda_functions.R
@@ -21,19 +21,15 @@ find_ndc <- function(name.category, name) {
 
 
 
-#' @title query
+#' @title Create and execute search parameters for use within FDA's openFDA
 #' @description Builds and executes queries. Allows for scraping all the information from drugs who have more than 100 entries (the maximum allowed by openFDA).  
 #' @param search.list What category your drug name falls under (Ex. "generic_name" or "brand_name")
 #' @param search.drug The name of the drug (Ex. "ibuprofen")
-#' @param limit PARAM_DESCRIPTION, Default: 100
-#' @param skip PARAM_DESCRIPTION, Default: 0
-#' @return OUTPUT_DESCRIPTION
-#' @details DETAILS
+#' @return A list with the results of scraping the API
+#' @details Output of this function is often very complicated and impossible to work with as some of the lists contain dataframes with list-columns
 #' @examples 
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' query("generic_name", "ibuprofen")
 #' }
 #' @seealso 
 #'  \code{\link[openfda]{fda_query}},\code{\link[openfda]{fda_api_key}},\code{\link[openfda]{fda_limit}},\code{\link[openfda]{fda_skip}},\code{\link[openfda]{fda_filter}},\code{\link[openfda]{fda_search}},\code{\link[openfda]{fda_exec}}
@@ -84,16 +80,15 @@ query <- function(search.list, search.drug) {
 }
 
 
-#' @title FUNCTION_TITLE
-#' @description FUNCTION_DESCRIPTION
-#' @param queried.df PARAM_DESCRIPTION
-#' @return OUTPUT_DESCRIPTION
+#' @title Handles the API's strange API output format
+#' @description Turn API's output into a list of clean (though not necessarily tidy) dataframes wherein each entry in one pull from the API
+#' @param queried.df The output of the \code{\link{query}} function
+#' @return List of clean dataframes. Each entry is one API pull
 #' @details DETAILS
 #' @examples 
 #' \dontrun{
-#' if(interactive()){
-#'  #EXAMPLE1
-#'  }
+#' x <- query("generic_name", "ibuprofen")
+#' dataframed_unique(x)
 #' }
 #' @seealso 
 #'  \code{\link[dplyr]{select}}
@@ -105,13 +100,9 @@ query <- function(search.list, search.drug) {
 #' @importFrom purrr map
 #' @importFrom tidyr unnest_longer
 dataframed_unique <- function(queried.df) {
-    # This function takes the out output of a drug query and turns it into a
-    # database where each row is different drug entry
     
     output_type <- inherits(queried.df, "data.frame")
 
-    # if the data came from multiple (looping queries I need to map this
-    # function onto multiple argumen    ts)
     if(output_type) {
         output_df <- queried.df %>% 
             dplyr::select(-openfda) %>% 
